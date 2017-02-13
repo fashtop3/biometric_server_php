7c194f4906a8ea5b8b37642b75a2f524c3784868
Fashola Ayodeji <fashtop3@gmail.com>
2017-02-13 19:08:03 +0100

    modified db password to null

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,46 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
         <script type="text/javascript" src="qrc:///qtwebchannel/qwebchannel.js"></script>
+        {{--<script type="text/javascript">--}}
+
+            {{--var capButton ;--}}
+            {{--var widget ;--}}
+
+            {{--// Initialise web page--}}
+            {{--function initialize() {--}}
+                {{--initialize_qt() ;--}}
+                {{--capButton = document.getElementById('capButton') ;--}}
+                {{--capButton.addEventListener('click', on_capButton_click, false) ;--}}
+            {{--}--}}
+
+            {{--// Initialise connection to QT C++--}}
+            {{--function initialize_qt() {--}}
+                {{--if (typeof qt != 'undefined') new QWebChannel(qt.webChannelTransport, function(channel) {--}}
+                    {{--widget = channel.objects.widget;--}}
+                {{--} );--}}
+            {{--}--}}
+
+            {{--// function, can be called from C++ or from Javascript--}}
+            {{--function doneCapturing(msg) {--}}
+                {{--alert(msg);--}}
+            {{--}--}}
+
+            {{--// Event handler for demonstration--}}
+            {{--function on_capButton_click(ev) {--}}
+                {{--//var x = ev.clientX - canvas.offsetLeft ;--}}
+                {{--//var y = ev.clientY - canvas.offsetTop ;--}}
+                {{--//addDot(x, y, 8, '#FF0000') ;--}}
+                {{--callCpp("Calling from html page");--}}
+            {{--}--}}
+
+            {{--// Javascript function passes parameter to C++--}}
+            {{--function callCpp(str) {--}}
+                {{--if (typeof widget !== 'undefined') {--}}
+                    {{--widget.jsStartCapturing(str) ;--}}
+                {{--}--}}
+            {{--}--}}
+
+        {{--</script>--}}
         <script type="text/javascript">
 
             var capButton ;
@@ -26,16 +66,13 @@
             }
 
             // function, can be called from C++ or from Javascript
-            function doneCapturing(msg) {
-                alert(msg);
+            function doneCapturing(cid, statusCode, statusText, responseData) {
+                alert(cid + " " + statusCode + " " + statusText);
             }
 
             // Event handler for demonstration
             function on_capButton_click(ev) {
-                //var x = ev.clientX - canvas.offsetLeft ;
-                //var y = ev.clientY - canvas.offsetTop ;
-                //addDot(x, y, 8, '#FF0000') ;
-                callCpp("Calling from html page");
+                callCpp("98289789024");
             }
 
             // Javascript function passes parameter to C++
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,6 @@ Route::get('/token', function() {
     return csrf_token();
 });
 
-Route::get('/data', 'CaptureController@index');
-Route::post('/data', 'CaptureController@store');
-
 Route::get('/', function () {
     return view('welcome');
 });
