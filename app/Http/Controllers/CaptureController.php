59071412602474204203af4bd7766e5574bc8bf6
Fashola Ayodeji <fashtop3@gmail.com>
2017-02-13 20:26:06 +0100

    protect data integrity ... avoid capture ambiguity

--- app/Http/Controllers/CaptureController.php
+++ app/Http/Controllers/CaptureController.php
@@ -7,11 +7,14 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Mockery\CountValidator\Exception;
 
 class CaptureController extends Controller
 {
     public function index()
     {
+//        dd(Auth::user());
         return Capture::all();
     }
 
@@ -19,16 +22,20 @@ class CaptureController extends Controller
     public function store(Request $request)
     {
         $input = $request->all();
-        $input['device_id'] = '1';
-        $input['cid'] = time(0);
+        $input['device_id'] = Auth::user()->id;
+//        $input['cid'] = time(0);
 
         try{
+            if(Capture::where('cid', $request->get('cid'))->first())
+            {
+                throw new Exception("Biometrics has been captured for the current user");
+            }
             Capture::create($input);
         }
         catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()]);
+            return response()->json(['error' => $e->getMessage()], 400);
         }
 
-        return response()->json(['message'=> "data saved"]);
+        return response()->json(['success'=> "data saved"], 200);
     }
 }
